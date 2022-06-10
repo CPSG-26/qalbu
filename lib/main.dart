f50c61ec4952097145b1c5c53d0626bb62608c02
Mochammad Bagus Insan <bagusinsan21@gmail.com>
2022-06-10 22:03:46 +0700

    add UI doa page
    add UI doa page

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:qalbu/common/colors.dart';
 import 'package:qalbu/common/text_styles.dart';
 import 'package:qalbu/common/utils.dart';
+import 'package:qalbu/presentation/pages/doa_page.dart';
 import 'package:qalbu/presentation/pages/home_page.dart';
+import 'package:qalbu/presentation/pages/list_doa_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final routes = <String, WidgetBuilder>{
+  ListDoaPage.routeName: (context) => const ListDoaPage(),
+  DoaPage.routeName: (context) => const DoaPage()
+};
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -20,14 +27,15 @@ class MyApp extends StatelessWidget {
           colorScheme: kColorScheme,
           primaryColor: kPrimary,
           textTheme: kTextTheme,
-          scaffoldBackgroundColor: Colors.white
-      ),
+          scaffoldBackgroundColor: Colors.white),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       navigatorObservers: [routeObserver],
       onGenerateRoute: (RouteSettings settings) {
         switch (settings.name) {
           case MyHomePage.routeName:
-            return MaterialPageRoute(builder: (_) => const MyHomePage(title: 'Flutter Demo Home Page'));
+            return MaterialPageRoute(
+                builder: (_) =>
+                    const MyHomePage(title: 'Flutter Demo Home Page'));
           default:
             return MaterialPageRoute(
               builder: (_) {
---
+++ lib/presentation/pages/doa_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class DoaPage extends StatefulWidget {
+  static const routeName = 'doa-page';
+
+  const DoaPage({Key? key}) : super(key: key);
+
+  @override
+  State<DoaPage> createState() => _DoaPageState();
+}
+
+class _DoaPageState extends State<DoaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0.0,
+        elevation: 5.0,
+        // ignore: prefer_const_constructors
+        backgroundColor: Color(0xFF629C59),
+        title: Row(
+          // ignore: sort_child_properties_last
+          children: const <Widget>[
+            Text(
+              'Doa Keluar Rumah',
+              // ignore: unnecessary_const
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+      ),
+      body: SafeArea(
+        child: Column(children: const <Widget>[
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 40),
+              child: Text(
+                'بِسْمِ اللَّهِ، تَوَكَّلْتُ عَلَى اللَّهِ، لاَ حَوْلَ وَلاَ قُوَّةَ إِلاَّ بِاللَّه',
+                textAlign: TextAlign.right,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(top: 10, left: 25, right: 20),
+              child: Text(
+                'Bismillahi tawakakaltu Alallah, Laa haula wala Quwwata illa Billah',
+                style: TextStyle(fontSize: 20, color: Colors.grey),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(top: 10, left: 25, right: 20),
+              child: Text(
+                'Dengan nama Allah, aku bertawakkal kepada Allah tiada daya dan kekuatan kecuali dengan Allah',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          )
+        ]),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
