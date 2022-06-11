c11f862ffbeae6ecf5ecd44f15d9af4c4a9c0bee
degalih <galihmramdan17@gmail.com>
2022-06-11 14:31:36 +0700

    create route for Al-quran feature and add Bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qalbu/common/colors.dart';
 import 'package:qalbu/common/text_styles.dart';
 import 'package:qalbu/common/utils.dart';
+import 'package:qalbu/injection.dart' as di;
+import 'package:qalbu/presentation/bloc/quran/quran_bloc.dart';
+import 'package:qalbu/presentation/bloc/quran_detail/quran_detail_bloc.dart';
 import 'package:qalbu/presentation/pages/doa_page.dart';
 import 'package:qalbu/presentation/pages/home_page.dart';
 import 'package:qalbu/presentation/pages/list_doa_page.dart';
+import 'package:qalbu/presentation/pages/quran_detail_page.dart';
+import 'package:qalbu/presentation/pages/quran_page.dart';
 import 'package:qalbu/presentation/pages/splash_screen.dart';
 import 'package:qalbu/presentation/pages/tasbih_page.dart';
 
 void main() {
+  di.init();
   runApp(const MyApp());
 }
 
@@ -19,44 +26,60 @@ final routes = <String, WidgetBuilder>{
 
 class MyApp extends StatelessWidget {
   static const String title = 'Qalbu';
+
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(
-          colorScheme: kColorScheme,
-          primaryColor: kPrimary,
-          textTheme: kTextTheme,
-          scaffoldBackgroundColor: Colors.white),
-      home: const SplashScreen(),
-      navigatorObservers: [routeObserver],
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case SplashScreen.routeName:
-            return MaterialPageRoute(builder: (_) => const SplashScreen());
-          case HomePage.routeName:
-            return MaterialPageRoute(builder: (_) => const HomePage());
-          case TasbihPage.routeName:
-            return MaterialPageRoute(builder: (_) => const TasbihPage());
-          case ListDoaPage.routeName:
-            return MaterialPageRoute(builder: (_) => const ListDoaPage());
-          case DoaPage.routeName:
-            return MaterialPageRoute(builder: (_) => const DoaPage());
-          default:
-            return MaterialPageRoute(
-              builder: (_) {
-                return const Scaffold(
-                  body: Center(
-                    child: Text('Page not found :('),
-                  ),
-                );
-              },
-            );
-        }
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => di.locator<QuranBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => di.locator<QuranDetailBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: title,
+        theme: ThemeData(
+            colorScheme: kColorScheme,
+            primaryColor: kPrimary,
+            textTheme: kTextTheme,
+            scaffoldBackgroundColor: Colors.white),
+        home: const SplashScreen(),
+        navigatorObservers: [routeObserver],
+        onGenerateRoute: (RouteSettings settings) {
+          switch (settings.name) {
+            case SplashScreen.routeName:
+              return MaterialPageRoute(builder: (_) => const SplashScreen());
+            case HomePage.routeName:
+              return MaterialPageRoute(builder: (_) => const HomePage());
+            case QuranPage.routeName:
+              return MaterialPageRoute(builder: (_) => const QuranPage());
+            case QuranDetailPage.routeName:
+              final id = settings.arguments as int;
+              return MaterialPageRoute(builder: (_) => QuranDetailPage(id: id));
+            case TasbihPage.routeName:
+              return MaterialPageRoute(builder: (_) => const TasbihPage());
+            case ListDoaPage.routeName:
+              return MaterialPageRoute(builder: (_) => const ListDoaPage());
+            case DoaPage.routeName:
+              return MaterialPageRoute(builder: (_) => const DoaPage());
+            default:
+              return MaterialPageRoute(
+                builder: (_) {
+                  return const Scaffold(
+                    body: Center(
+                      child: Text('Page not found :('),
+                    ),
+                  );
+                },
+              );
+          }
+        },
+      ),
     );
   }
 }
