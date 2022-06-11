b32065af137f35a619e77cec6a63b516bb021ce8
degalih <galihmramdan17@gmail.com>
2022-06-11 14:27:04 +0700

    Create quran_page.dart

---
+++ lib/presentation/pages/quran_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qalbu/presentation/bloc/quran/quran_bloc.dart';
+import 'package:qalbu/presentation/widgets/quran_list.dart';
+
+class QuranPage extends StatefulWidget {
+  static const routeName = 'al-quran';
+
+  const QuranPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuranPage> createState() => _QuranPageState();
+}
+
+class _QuranPageState extends State<QuranPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      context.read<QuranBloc>().add(FetchQuranList());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Al-Quran'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              BlocBuilder<QuranBloc, QuranState>(builder: (context, state) {
+                if (state is QuranLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is QuranHasData) {
+                  return QuranList(quranList: state.quranList);
+                } else {
+                  return const Text('Failed');
+                }
+              })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
