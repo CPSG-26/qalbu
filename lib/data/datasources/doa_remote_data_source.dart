a750ceb1de5130787c4adadc189441efea3f894b
rizkiramadanil <rizkiramadanil000@gmail.com>
2022-06-15 11:22:08 +0700

    create datasources for doa feature

---
+++ lib/data/datasources/doa_remote_data_source.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:qalbu/common/exception.dart';
+import 'package:qalbu/data/models/doa_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class RemoteDataSource {
+  Future<List<DoaModel>> getDoaList();
+  Future<DoaModel> getDoaDetail(String id);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  static const baseURL = 'https://doa-doa-api-ahmadramadhan.fly.dev/api';
+
+  final http.Client client;
+
+  RemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<List<DoaModel>> getDoaList() async {
+    final response = await client.get(Uri.parse(baseURL));
+
+    if (response.statusCode == 200) {
+      return doaModelFromJson(response.body);
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<DoaModel> getDoaDetail(String id) async {
+    final response = await client.get(Uri.parse('$baseURL/$id'));
+
+    if (response.statusCode == 200) {
+      return DoaModel.fromJson(json.decode(response.body)[0]);
+    } else {
+      throw ServerException();
+    }
+  }
+}
