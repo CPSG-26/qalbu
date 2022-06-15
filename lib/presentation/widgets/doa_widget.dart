087936f854df8b5dff9d061b97f711db45d41af6
rizkiramadanil <rizkiramadanil000@gmail.com>
2022-06-15 12:24:09 +0700

    update doa widget

--- lib/presentation/widgets/doa_widget.dart
+++ lib/presentation/widgets/doa_widget.dart
@@ -1,79 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:qalbu/common/colors.dart';
-import 'package:qalbu/data/models/doa_data.dart';
+import 'package:qalbu/domain/entities/doa.dart';
+import 'package:qalbu/presentation/pages/doa_detail_page.dart';
 
 class DoaWidget extends StatelessWidget {
-  const DoaWidget({Key? key}) : super(key: key);
+  final Doa doa;
+
+  const DoaWidget(this.doa);
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-      context: context,
-      removeTop: true,
-      child: ListView.builder(
-        primary: false,
-        shrinkWrap: true,
-        itemBuilder: (context, index) {
-          final Doa doa = doaData[index];
-          return InkWell(
-            onTap: () {},
-            child: Card(
-              child: Padding(
-                padding:
-                    const EdgeInsets.only(top: 4, right: 8, bottom: 4, left: 8),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 10,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            doa.title,
-                            style: GoogleFonts.poppins(
-                                fontSize: 15, fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Expanded(flex: 1, child: FavoriteButton()),
-                  ],
+    return SizedBox(
+      height: 60,
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            DoaDetailPage.routeName,
+            arguments: doa.id,
+          );
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 8, left: 8),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                doa.doa ?? '-',
+                style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500
                 ),
               ),
             ),
-          );
-        },
-        itemCount: doaData.length,
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({Key? key}) : super(key: key);
-
-  @override
-  _FavoriteButtonState createState() => _FavoriteButtonState();
-}
-
-class _FavoriteButtonState extends State<FavoriteButton> {
-  bool isFavorite = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isFavorite ? Icons.bookmark : Icons.bookmark_border,
-        color: kSecondary,
-        size: 32.0,
-      ),
-      onPressed: () {
-        setState(() {
-          isFavorite = !isFavorite;
-        });
-      },
     );
   }
 }
