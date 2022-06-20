79c8c8a12281be6febe93abc9ad2001b32ff1f74
rizkiramadanil <rizkiramadanil000@gmail.com>
2022-06-20 13:03:15 +0700

    create widgets for arah kiblat feature

---
+++ lib/presentation/widgets/kiblat_widget.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
+
+class KiblatWidget extends StatelessWidget {
+  const KiblatWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FlutterQiblah.qiblahStream,
+      builder: (_, AsyncSnapshot<QiblahDirection> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final qiblatDirection = snapshot.data!;
+
+        return Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Transform.rotate(
+              angle: (qiblatDirection.direction * (pi / 180) * -1),
+              child: Image.asset('assets/images/compass.png'),
+            ),
+            Transform.rotate(
+              angle: (qiblatDirection.qiblah * (pi / 180) * -1),
+              child: Image.asset(
+                'assets/images/needle.png',
+                scale: 1.2,
+              ),
+            ),
+          ],
+        );
+      }
+    );
+  }
+}
